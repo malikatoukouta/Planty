be2b9c065455dab3e94de302097ac2a91017f7c1
Abdourmalik Adamou <abdourmalik@gmail.com>
2023-06-21 13:41:53 +0200

    second commit

--- functions.php
+++ functions.php
@@ -5,3 +5,6 @@ function theme_enqueue_styles(){
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
+
+add_filter('wpc7_autop_or_not','__return_false');
+

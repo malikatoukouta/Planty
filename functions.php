1258468cb70d38194085800f7766cd7a90790388
malik <abdourmalik@gmail.com>
2023-07-03 14:49:54 +0200

    last commit

--- functions.php
+++ functions.php
@@ -8,3 +8,14 @@ function theme_enqueue_styles(){
 
 add_filter('wpc7_autop_or_not','__return_false');
 
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+function add_extra_item_to_nav_menu( $items, $args ) {
+    if (is_user_logged_in()) {
+        $items = '<li id="menu-item-49" class="menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu"><a href="http://localhost/planty/nous-rencontrer/" class = "hfe-menu-item">Nous rencontrer</a></li>
+        <li class="menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu"><a href="http://localhost/planty/">Admin</a></li>
+        <li id="menu-item-48" class="menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu"><a href="http://localhost/planty/precommande/" class = "hfe-menu-item">Commander</a></li>
+        ';
+    }
+    
+    return $items;
+}
